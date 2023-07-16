77acd8a7fa9614abfca87de3eb6659aa8e85af71
Clarisse Casi Cariño <94608289+clarissecasicarino@users.noreply.github.com>
2023-07-16 20:26:33 +0800

    Add package_carousel component

--- slices/Carousel/index.tsx
+++ slices/Carousel/index.tsx
@@ -1,24 +1,54 @@
 import { Content } from "@prismicio/client";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import {
+  PrismicRichText,
+  SliceComponentProps,
+  PrismicRichTextProps,
+} from "@prismicio/react";
+import Head from "next/head";
 
 export type PackageCarouselProps =
   SliceComponentProps<Content.PackageCarouselSlice>;
 
+const CustomCheckMark = () => <span className="mr-2">&#x2713;</span>;
+
+const components: PrismicRichTextProps["components"] = {
+  listItem: ({ children }) => (
+    <li className="flex items-start">
+      <CustomCheckMark />
+      <span className="ml-1 font-body">{children}</span>
+    </li>
+  ),
+};
+
 const PackageCarousel = ({ slice }: PackageCarouselProps): JSX.Element => {
   return (
-    <div className="flex flex-row space-x-2 md:space-x-4">
-      {slice.items.map((item, index) => (
-        <div
-          key={index}
-          className={'rounded-3xl h-40 w-80'}
-          style={{ color: item.package_bg_color ?? "" }}
-        >
-          <p className="font-bold">{item.package_title}</p>
-          <p>{item.package_price}</p>
-          <PrismicRichText field={item.package_info} />
-        </div>
-      ))}
-    </div>
+    <>
+      <Head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+      </Head>
+      <div className="flex flex-row space-x-2 md:space-x-4">
+        {slice.items.map((item, index) => (
+          <div
+            key={index}
+            className={`rounded-3xl border h-80 w-72`}
+            style={{ backgroundColor: item.package_bg_color ?? "white" }}
+          >
+            <div className="px-4 py-4">
+              <p key={index} className="font-body">
+                {item.package_title}
+              </p>
+              <p className="text-5xl font-display font-extrabold">
+                {item.package_price}
+              </p>
+              <PrismicRichText
+                field={item.package_info}
+                components={components}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./slices/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        display: ['"Wix Madefor Display"', "sans-serif"],
+        body: ["Playfair", "serif"],
+      },
+      colors: {
+        electricBlue: {
+          50: "#E5EAFF",
+          100: "#B0BFFF",
+          200: "#7B94FF",
+          300: "#4D6EFF",
+          400: "#244DFF",
+          500: "#0030FF",
+        },
       },
     },
   },
   plugins: [],
-}
+};
