f2c15e4a69985250ff64405c1e1fb8d1ea7b1526
Clarisse Casi Cariño <94608289+clarissecasicarino@users.noreply.github.com>
2023-07-23 10:16:25 +0800

    Remove comments

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,8 @@ import { components } from "@/slices";
 
 export default async function Home() {
   const client = createClient();
-  // const page = await client.getSingle("homepage");
+
   const document = await client.getByUID('homepage', 'home-page');
 
   return <SliceZone slices={document.data.slices} components={components} />;
 }
-
-// export default async function Home() {
-//   const client = createClient();
-
-//   const document = await client.getSingle('home_page');
-
-//   return <section>{document.uid}</section>;
-// }
