11a43ff72b57d0f4aedda66e8bf68db661dfc456
Clarisse Casi Cariño <94608289+clarissecasicarino@users.noreply.github.com>
2023-07-28 19:12:29 +0800

    Update button circular styling

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -19,8 +19,8 @@ export default function Button({
       break;
     case "circular":
       buttonStyle =
-        "bg-tertiaryPurple w-32 hover:border-green-500 active:bg-green-500 rounded-full";
-      textStyle = `text-electricBlue-500 ${"font-body"} font-medium`;
+        "bg-tertiaryPurple w-32 hover:bg-primaryPurple active:bg-primaryPurple rounded-full";
+      textStyle = `text-black group-hover:text-white group-active:text-white ${"font-body"} font-medium`;
       break;
     case "special":
       buttonStyle = "w-full tablet:w-36 bg-black hover:bg-pastelBlue active:bg-primaryPurple rounded-lg";
