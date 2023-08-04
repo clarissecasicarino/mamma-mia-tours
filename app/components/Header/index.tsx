ee4dc733c82aeb451fda358cb8a86bfa3ceaf92b
Clarisse Casi Cariño <94608289+clarissecasicarino@users.noreply.github.com>
2023-08-04 20:14:18 +0800

    Update header's padding

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -10,27 +10,33 @@ export default function Header(): ReactElement {
   };
 
   return (
-    <header
-      className={`${styles.header} px-6 tablet:px-8 laptop:px-10 desktop:px-[248px]`}
-    >
-      <div className={styles.logo}>
-        <Link href="/">Mamma Mia Tours</Link>
-      </div>
-      <nav className={`${styles.nav} ${isMenuOpen ? styles.open : ""}`}>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-        <button className={styles.signup}>Sign Up</button>
-      </nav>
-      <div className={styles.hamburger} onClick={handleMenuToggle}>
-        <div
-          className={`${styles.bar} text-12 ${isMenuOpen ? styles.open : ""}`}
-        />
-        <div
-          className={`${styles.bar} text-12 ${isMenuOpen ? styles.open : ""}`}
-        />
-        <div
-          className={`${styles.bar} text-12 ${isMenuOpen ? styles.open : ""}`}
-        />
+    <header className={`${styles.header}`}>
+      <div className="flex justify-between items-center px-6 tablet:px-8 laptop:px-10 desktop:px-[248px]">
+        <div className={`${styles.logo}`}>
+          <Link href="/">Mamma Mia Tours</Link>
+        </div>
+        <nav
+          className={`${styles.nav} ${
+            isMenuOpen ? styles.open : ""
+          } flex justify-end`}
+        >
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+          <button className={styles.signup}>Sign Up</button>
+        </nav>
+        {/*  */}
+        <div className={styles.hamburger} onClick={handleMenuToggle}>
+          <div
+            className={`${styles.bar} text-12 ${isMenuOpen ? styles.open : ""}`}
+          />
+          <div
+            className={`${styles.bar} text-12 ${isMenuOpen ? styles.open : ""}`}
+          />
+          <div
+            className={`${styles.bar} text-12 ${isMenuOpen ? styles.open : ""}`}
+          />
+        </div>
+        {/*  */}
       </div>
     </header>
   );
