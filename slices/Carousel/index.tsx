1a5b34274d4239895f02547982568952bda4b39b
Clarisse Casi Cariño <94608289+clarissecasicarino@users.noreply.github.com>
2023-08-04 17:39:57 +0800

    Remove head from carousel

--- slices/Carousel/index.tsx
+++ slices/Carousel/index.tsx
@@ -5,7 +5,6 @@ import {
   SliceComponentProps,
   PrismicRichTextProps,
 } from "@prismicio/react";
-import Head from "next/head";
 import Button from "@/app/components/Button";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -33,46 +32,41 @@ const PackageCarousel = ({ slice }: PackageCarouselProps): JSX.Element => {
   const anchorId = slice.primary.anchor_id;
 
   return (
-    <>
-      <Head>
-        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-      </Head>
-      <div
-        id={anchorId ?? ""}
-        className="flex flex-col laptop:flex-row gap-x-6 justify-center gap-y-6"
-      >
-        {slice.items.map((item, index) => (
-          <div
-            key={index}
-            className={`rounded-3xl border h-[330px] w-full laptop:w-[298px]`}
-            style={{ backgroundColor: item.package_bg_color ?? "white" }}
-          >
-            <div className="px-6 py-5 flex flex-col gap-y-2">
-              <p key={index} className="font-body text-12 laptop:text-14">
-                {item.package_title}
-              </p>
-              <p className="font-display font-extrabold py-5 text-40">
-                {item.package_price}
-              </p>
-              <section className="py-1">
-                <PrismicRichText
-                  field={item.package_info}
-                  components={components}
-                />
-              </section>
-              <div className="flex pt-2 justify-center">
-                <Button
-                  label="Book Now"
-                  type="rectangle"
-                  onClick={() => notify(item.package_title)}
-                />
-              </div>
-              <ToastContainer />
+    <div
+      id={anchorId ?? ""}
+      className="flex flex-col laptop:flex-row gap-x-6 justify-center gap-y-6"
+    >
+      {slice.items.map((item, index) => (
+        <div
+          key={index}
+          className={`rounded-3xl border h-[330px] w-full laptop:w-[298px]`}
+          style={{ backgroundColor: item.package_bg_color ?? "white" }}
+        >
+          <div className="px-6 py-5 flex flex-col gap-y-2">
+            <p key={index} className="font-body text-12 laptop:text-14">
+              {item.package_title}
+            </p>
+            <p className="font-display font-extrabold py-5 text-40">
+              {item.package_price}
+            </p>
+            <section className="py-1">
+              <PrismicRichText
+                field={item.package_info}
+                components={components}
+              />
+            </section>
+            <div className="flex pt-2 justify-center">
+              <Button
+                label="Book Now"
+                type="rectangle"
+                onClick={() => notify(item.package_title)}
+              />
             </div>
+            <ToastContainer />
           </div>
-        ))}
-      </div>
-    </>
+        </div>
+      ))}
+    </div>
   );
 };
 
