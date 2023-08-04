00216137cd0e58486220165c202d2e63aa3b70af
Clarisse Casi Cariño <94608289+clarissecasicarino@users.noreply.github.com>
2023-08-04 10:38:49 +0800

    Add react-scroll in FirstComponent

--- app/components/FirstComponent/index.tsx
+++ app/components/FirstComponent/index.tsx
@@ -1,13 +1,16 @@
-"use client"
+"use client";
 
 import React from "react";
 import MainIcon from "../../assets/images/main-icon.png";
 import Image from "next/image";
-import Button from "../Button";
 import styles from "./styles.module.css";
 import { TypeAnimation } from "react-type-animation";
+import { Link } from "react-scroll";
+import Button from "../Button";
+
 
 export default function FirstComponent() {
+
   return (
     <div className="flex flex-col laptop:flex-row pt-16 gap-x-4 justify-center">
       <div
@@ -18,10 +21,10 @@ export default function FirstComponent() {
         >
           <TypeAnimation
             sequence={[
-                `Life is short, the world is wide.\nI want to make some memories.`,
-                5000,
-                '',
-              ]}
+              `Life is short, the world is wide.\nI want to make some memories.`,
+              5000,
+              "",
+            ]}
             wrapper="span"
             speed={50}
             omitDeletionAnimation={true}
@@ -29,7 +32,13 @@ export default function FirstComponent() {
           />
         </p>
         <div className={`flex justify-center tablet:justify-start`}>
-          <Button label="Discover More" type="circular" />
+           <Link
+            to="packageCarousel"
+            smooth={true}
+            duration={500}
+          >
+            <Button label="Discover More" type="circular" />
+          </Link>
         </div>
       </div>
       <div className="pt-6 w-full desktop:w-[29rem]">
