3d736977b6113bba0f710bf77fe120354620e6a5
Clarisse Casi Cariño <94608289+clarissecasicarino@users.noreply.github.com>
2023-08-06 13:36:07 +0800

    Add link in document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1.00001, viewport-fit=cover"
-        />
+        <link rel="icon" href="../app/icon.png" sizes="32x32" />
       </Head>
       <body>
         <Main />
