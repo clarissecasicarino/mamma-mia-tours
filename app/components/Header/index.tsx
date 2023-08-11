ce6688dcd9280ebbc89bc3fad2dd6adb19646082
Clarisse Casi Cariño <94608289+clarissecasicarino@users.noreply.github.com>
2023-08-11 19:44:18 +0800

    Fix header, make it responsive

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,14 +1,16 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { ReactElement, useCallback, useState } from "react";
 import styles from "./styles.module.css";
 import { useRouter } from "next/navigation";
+import { HiViewGridAdd, HiX } from "react-icons/hi";
+import { IconContext } from "react-icons";
 
 export default function Header(): ReactElement {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleMenuToggle = () => {
-    setIsMenuOpen((prevState) => !prevState);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   const router = useRouter();
@@ -18,34 +20,48 @@ export default function Header(): ReactElement {
   }, [router]);
 
   return (
-    <header className={`${styles.header}`}>
-      {/* note: dont change desktop:px-[232px] */}
-      <div className="flex justify-between items-center px-2 tablet:px-4 laptop:px-6 desktop:px-[232px]">
-        <div className={`${styles.logo} text-22 laptop:text-24`}>
+    <header className="bg-secondaryPurple sticky top-0 z-0 font-body">
+      <div className="container mx-auto py-4 flex justify-between items-center">
+        <div className="text-22 laptop:text-24 font-bold px-2 text-primaryPurple pl-6 tablet:pl-8 laptop:pl-10">
           <Link href="/">Mamma Mia Tours</Link>
         </div>
-        <nav
-          className={`${styles.nav} ${
-            isMenuOpen ? styles.open : ""
-          } flex justify-end`}
-        >
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
-          <button className={styles.signup} onClick={navigateToSignUpPage}>Sign Up</button>
+        <nav className="hidden tablet:flex space-x-4 items-center pr-0 tablet:pr-6 laptop:pr-8 text-14 tablet:text-16 font-medium">
+          <Link href="/about" className="hover:text-primaryPurple">
+            About
+          </Link>
+          <Link href="/contact" className="hover:text-primaryPurple">
+            Contact
+          </Link>
+          <button className={`${styles.signup}`} onClick={navigateToSignUpPage}>
+            Sign Up
+          </button>
         </nav>
-        {/*  */}
-        <div className={styles.hamburger} onClick={handleMenuToggle}>
-          <div
-            className={`${styles.bar} text-12 ${isMenuOpen ? styles.open : ""}`}
-          />
-          <div
-            className={`${styles.bar} text-12 ${isMenuOpen ? styles.open : ""}`}
-          />
-          <div
-            className={`${styles.bar} text-12 ${isMenuOpen ? styles.open : ""}`}
-          />
+        <div className="tablet:hidden">
+          <button className="text-primaryPurple p-2 pr-6" onClick={toggleMenu}>
+            <IconContext.Provider value={{ size: "1.25rem" }}>
+              {isMenuOpen ? <HiX /> : <HiViewGridAdd />}
+            </IconContext.Provider>
+          </button>
         </div>
       </div>
+      {isMenuOpen && (
+        <div className="flex flex-col laptop:hidden bg-secondaryPurple py-2 justify-center items-center">
+          <Link href="/about" className="hover:text-primaryPurple py-2">
+            About
+          </Link>
+          <Link href="/contact" className="hover:text-primaryPurple py-2">
+            Contact
+          </Link>
+          <div className="py-2">
+            <button
+              className={`${styles.signup}`}
+              onClick={navigateToSignUpPage}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
